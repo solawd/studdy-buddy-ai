460cb3d033adc3e2e3ec9affa4a7be945512c3d8
Solomon Apenya <solawd@gmail.com>
2025-12-12 16:11:15 +0000

    Add new llm client

---
+++ src/llm/llm_client.py
@@ -0,0 +1,29 @@
+from langchain_groq import ChatGroq
+import os
+from langchain_openai.chat_models import ChatOpenAI
+from src.config.settings import settings
+
+
+class LLMModel:
+    """Handles different LLM models"""
+
+    def __init__(self, model="gpt-5-nano"):
+        if model in ["gpt-5", "gpt-5-mini", "gpt-5-nano"]:
+            self.llm = self._get_openai_client(model)
+        else:
+            self.llm = self._get_groq_client(model)
+
+    def _get_groq_client(self,model='llama-3.1-8b-instant'):
+        return ChatGroq(
+            api_key=settings.GROQ_API_KEY,
+            model=model,
+            temperature=settings.TEMPERATURE
+        )
+
+    def _get_openai_client(self, model='gpt-5-nano'):
+        key = os.getenv("OPEN_AI_API_KEY")
+        return ChatOpenAI(
+            model=model,
+            temperature=settings.TEMPERATURE,
+            api_key=key
+        )
